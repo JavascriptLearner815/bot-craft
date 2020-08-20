9ecb9d209e47a7b3d6f482fbdf85fe0c51491ee3
Dean Summer <deanlovesmargie@gmail.com>
2020-08-20 18:56:02 -0500

    blah blah blah

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
+const fs = require('fs');
+
 const { prefix, token } = require('./config.json'); // config.json file includes token, so not including in repo.
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+const cooldowns = new Discord.Collection();
+
+const commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('message', message => {
-	if (message.content === `${prefix}server`) {
-	    message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}\nCreated: ${message.guild.createdAt}\nLocation: ${message.guild.region}`);
-	} else if (message.content === `${prefix}user-info`) {
-            message.channel.send(`Username: ${message.author.username}\nID: ${message.author.id}`);
-	}
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName)) return;
+    
+    const command = client.commands.get(commandName);
+
+    if (message.channel.type === 'dm') return;
+
+    if (command.args && !args.length) {
+        let reply = `${message.author}, you need to supply arguments.`;
+
+        if (command.usage) {
+            reply = `Use the command like this: \`${prefix}${command.name} ${command.usage}\``;
+        }
+
+        return message.channel.send(reply);
+    }
+
+    try {
+	    command.execute(message, args);
+    } catch (error) {
+	    console.error('Error executing command:', error);
+	    message.reply('there was an error trying to execute that command!');
+    }
 });
 
 client.login(token);
