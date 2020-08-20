6c2453154270bc80519192827d893eab248553e5
Dean Summer <deanlovesmargie@gmail.com>
2020-08-20 18:01:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { prefix, token } = require('./config.json');
+const { prefix, token } = require('./config.json'); // config.json file includes token, so not including in repo.
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
