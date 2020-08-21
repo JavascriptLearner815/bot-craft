35da22ddcc7a06232f2232cb7302d915fc07fa7b
Dean Summer <deanlovesmargie@gmail.com>
2020-08-20 20:35:25 -0500

    fixes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-const { prefix, token, client, secret } = require('./config.json'); // config.json file includes token, so not including in repo.
+const { prefix, token, clientID, secret } = require('./config.json'); // config.json file includes token, so not including in repo.
 
 const Discord = require('discord.js');
-const client = new Discord.Client({ ws: { intents: ['DIRECT_MESSAGE_TYPING', 'DIRECT_MESSAGES', 'DIRECT_MESSAGE_REACTIONS'] } });
+const client = new Discord.Client();
 
 const cooldowns = new Discord.Collection();
 
