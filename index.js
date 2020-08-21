698c5dccd4bfe2d12113429f04eba333ce52cf1c
Dean Summer <deanlovesmargie@gmail.com>
2020-08-20 19:56:01 -0500

    new features

--- index.js
+++ index.js
@@ -26,10 +26,11 @@ client.on('message', message => {
 
     const args = message.content.slice(prefix.length).trim().split(' ');
     const commandName = args.shift().toLowerCase();
-
-    if (!client.commands.has(commandName)) return;
     
-    const command = client.commands.get(commandName);
+    const command = client.commands.get(commandName)
+        || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+    if (!command) return;
 
     if (message.channel.type === 'dm') return;
 
@@ -41,6 +42,26 @@ client.on('message', message => {
         }
 
         return message.channel.send(reply);
+    }
+
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`Command on cooldown! ${timeLeft.toFixed(1)}`);
+        }
+
+        timestamps.set(message.author.id, now);
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
     }
 
     try {
